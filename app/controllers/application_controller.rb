48a4d74d8f970de14aec8a7f15cd649558a0c859
Bruno Aguiar <evolutionsyndicate@gmail.com>
2022-12-12 20:07:55 -0500

    Rails Setup

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,10 @@
+class ApplicationController < ActionController::API
+    include ActionController::Cookies
+
+# Code to test middleware/cookie configuration
+#     def hello_world
+#         session[:count] = (session[:count] || 0) + 1
+#         render json: { count: session[:count] }
+#     end
+
+end
