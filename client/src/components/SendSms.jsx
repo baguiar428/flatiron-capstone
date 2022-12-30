0c53395bfb0e074249f7591322d677818f4d4228
Bruno Aguiar <evolutionsyndicate@gmail.com>
2022-12-30 16:58:41 +0000

    Sending SMS from form in portal works

--- client/src/components/SendSms.jsx
+++ client/src/components/SendSms.jsx
@@ -1,84 +1,111 @@
-import React from "react";
+import React, {useState} from "react";
 import { AppBar, Toolbar, Typography, TextField, Box, Button } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 
 function SendSms() {
+
+    const navigate = useNavigate();
+
+    const [formData, setFormData] = useState({
+        from: '+18313185894',
+        to: '',
+        body: ''
+    })
+
+    function handleChange(e) {
+        const {name, value} = e.target 
+        setFormData({...formData, [name]: value})
+      }
+
+    async function handleSubmit(e) {
+        e.preventDefault()
+        const smsText = {
+            ...formData
+        }
+
+        await fetch('/text', {
+            method: 'POST',
+            headers: {
+             "Content-Type": "application/json"
+            },
+            body: JSON.stringify(smsText)
+        })
+        .then(resp => {
+            if(resp.ok){
+                console.log('Text Sent')
+            }else{
+                resp.json().then(data => {
+                    console.log('Errors:', data)
+                })
+            }
+        })
+
+        await fetch('/text')
+        .then(resp => resp.json())
+        .then(console.log('SMS Sent OK'))
+    }
+
+    function backToDashboard() {
+        navigate('/dashboard')
+    }
+
     return (
         <>
-              <AppBar sx={{
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center'
-      }}>
-        <Toolbar>
-          <Typography variant="h2">
-            Client Info Form
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <Box sx={{
-        marginTop: 14,
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center'
-      }}>
-        <form onSubmit={handleSubmit}>
-          <TextField
-            style={{ width: "200px", margin: "5px" }}
-            type="text"
-            label="First Name"
-            name="first_name"
-            value={formData.first_name}
-            onChange={handleChange}
-            variant="outlined"
-          />
-          <br />
-          <TextField
-            style={{ width: "200px", margin: "5px" }}
-            type="text"
-            label="Last Name"
-            name="last_name"
-            value={formData.last_name}
-            onChange={handleChange}
-            variant="outlined"
-          />
-          <br />
-          <TextField
-            style={{ width: "200px", margin: "5px" }}
-            type="text"
-            label="Phone Number"
-            name="phone_number"
-            value={formData.phone_number}
-            onChange={handleChange}
-            variant="outlined"
-          />
-          <br />
-          <TextField
-            style={{ width: "200px", margin: "5px" }}
-            type="text"
-            label="E-Mail"
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
-            variant="outlined"
-          />
-          <br />
-          <Button
-            sx={{ ml: 1 }}
-            variant="contained"
-            color="secondary"
-            onClick={backToDashboard}>
-            Cancel
-          </Button>
-          <Button
-            type="submit"
-            sx={{ ml: 4 }}
-            variant="contained"
-            color="primary">
-            Save
-          </Button>
-        </form>
-      </Box>
+            <AppBar sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center'
+            }}>
+                <Toolbar>
+                    <Typography variant="h2">
+                        Send SMS Promotion
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+            <Box sx={{
+                marginTop: 14,
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center'
+            }}>
+                <form onSubmit={handleSubmit}>
+                    <TextField
+                        style={{ width: "200px", margin: "5px" }}
+                        type="text"
+                        label="To:"
+                        name="to"
+                        value={formData.to}
+                        onChange={handleChange}
+                        variant="outlined"
+                    />
+                    <br />
+                    <TextField
+                        style={{ width: "200px", margin: "5px" }}
+                        type="text"
+                        label="Message"
+                        name="body"
+                        value={formData.body}
+                        onChange={handleChange}
+                        variant="outlined"
+                    />
+                    <br />
+                    <Button
+                        sx={{ ml: 1 }}
+                        variant="contained"
+                        color="secondary"
+                        onClick={backToDashboard}>
+                        Cancel
+                    </Button>
+                    <Button
+                        type="submit"
+                        sx={{ ml: 4 }}
+                        variant="contained"
+                        color="primary">
+                        Send
+                    </Button>
+                </form>
+            </Box>
         </>
     )
 }
