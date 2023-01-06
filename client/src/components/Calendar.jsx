eb7f8a6b9dd70573a19c3de6564ffe698693b4a4
Bruno Aguiar <evolutionsyndicate@gmail.com>
2023-01-06 14:45:35 +0000

    Added Exit button on calendar component

--- client/src/components/Calendar.jsx
+++ client/src/components/Calendar.jsx
@@ -1,9 +1,19 @@
 import React from "react";
 import { InlineWidget } from "react-calendly";
+import { useNavigate } from "react-router-dom";
+import Button from '@mui/material/Button';
 
 function Calendar() {
+
+    const navigate = useNavigate();
+
+    function goHome() {
+        navigate('/');
+    }
+
     return (
         <>
+            <Button sx={{ ml: 2, mt: 2}} variant="contained" onClick={goHome}>Exit</Button>
             <InlineWidget url="https://calendly.com/baguiar428" />
         </>
     )
